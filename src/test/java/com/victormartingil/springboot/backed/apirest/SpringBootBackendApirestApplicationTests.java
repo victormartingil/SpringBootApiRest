1ad1835d1d4fcf22a19ebde3ada85edddd9dec05
victormartingil <victormartingil@gmail.com>
2020-11-13 19:33:49 +0100

    first commit

---
+++ src/test/java/com/victormartingil/springboot/backed/apirest/SpringBootBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victormartingil.springboot.backed.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
